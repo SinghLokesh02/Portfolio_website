e7d0905c6599e90d5c17923780425279f82b8449
Lokesh <lokeshsingh7695@gmail.com>
2023-07-23 06:55:31 +0530

    Initial commit

---
+++ db_connect.php
@@ -0,0 +1,10 @@
+<?php
+$conn = mysqli_connect('localhost','root','','portfolio_contact');
+if($conn){
+    
+}
+// echo 'Connection is Successful';
+else{
+        die("error".mysqli_connect_error());
+}
+?>
